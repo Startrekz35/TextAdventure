38ab31311551379a0b847f62479492fe42521de2
Startrekz35 <burkz3905@gmail.com>
2022-02-19 08:38:27 -0600

    Updated Text

--- main.cpp
+++ main.cpp
@@ -7,25 +7,26 @@ std::string name;
 int main()
 {
     //Intro with a newline as a way to press "enter"
-    std::cout << "Welcome to Text Game Unlimited" << std::endl;
+    std::cout << "???: Welcome to Text Game Unlimited" << std::endl;
     std::cout << std::endl; 
 
     //Used a newline as a way to press "enter"
-    std::cout << "Press Enter to Continue" << std::endl;
+    std::cout << "???: Press Enter to Continue" << std::endl;
     while (std::cin.get()!= '\n'); 
     
     //Beginning of Loop
     do
     {
-    std::cin.ignore(256, '\n'); //Clears stream buff to be able to restart loop from beginning without program skipping getline() input
-    std::cout << "Enter your name and press Enter to Continue" << std::endl;
+    //Clears stream buff to be able to restart loop from beginning without program skipping getline() input
+    std::cin.ignore(256, '\n');
+    std::cout << "???: Enter your name and press Enter to Continue" << std::endl;
     
     //Can enter both first and last name in one go
     std::getline(std::cin, name);
     std::cout << std::endl;
 
     //Asks you if name is correct and shows you name
-    std::cout << "Is this correct? " << std::endl;
+    std::cout << "???: Is this correct? " << std::endl;
     std::cout << name << std::endl;
     std::cout << std::endl;
 
@@ -35,9 +36,22 @@ int main()
      } while (restart == 'n' && 'N');
     
     std::cout << std::endl;
-    std::cout << "Thank you, let us continue." << std::endl;
+    std::cout << "???: Thank you, let us continue." << std::endl;
 
     std::cout << std::endl;
+    while (std::cin.get()!= '\n');
 
+    std::cout << "???: Your adventure starts off as a mere nobody.." << std::endl;
+    while (std::cin.get()!= '\n');
+
+    std::cout << "???Who later leaves their mark on time and space..." << std::endl;
+    while (std::cin.get()!= '\n');
+
+    std::cout << "???: In a Universe SHATTERING WAY!" << std::endl;
+    while (std::cin.get()!= '\n');
+
+    std::cout << "???: I will quickly go over the rules of combat." << std::endl;
+    while (std::cin.get()!= '\n');
+    
     return 0;
 }
